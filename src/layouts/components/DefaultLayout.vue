dfd7bd80b962e47fef3a555e9960ae401f5c5845
Daniel Pittman <dpdanpittman@gmail.com>
2024-03-30 11:53:22 -0600

    update favicon and descriptions

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -242,18 +242,18 @@ dayjs()
           {{ $t('module.sponsors') }}
         </div>
         <a
-          href="https://osmosis.zone"
+          href="https://qwoyn.studio"
           target="_blank"
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
           <img
-            src="https://ping.pub/logos/osmosis.jpg"
+            src="https://raw.githubusercontent.com/cosmos/chain-registry/master/qwoyn/images/qwoyn.png"
             class="w-6 h-6 rounded-full mr-3"
           />
           <div
             class="text-sm capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
-            Osmosis
+            Qwoyn
           </div>
         </a>
         <a
